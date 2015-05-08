ef15fbe0c0043cce4f4aeb9691ea914095033abc
Alex Xandra Albert Sim <bertzzie@gmail.com>
2015-05-08 12:10:39 +0700

    Start theming. Lunch break.

--- app/controllers/init.go
+++ app/controllers/init.go
@@ -7,4 +7,8 @@ func init() {
 	revel.InterceptMethod((*DBRController).Begin, revel.BEFORE)
 	revel.InterceptMethod((*DBRController).Commit, revel.AFTER)
 	revel.InterceptMethod((*DBRController).RollBack, revel.FINALLY)
+
+	revel.TemplateFuncs["config"] = func(key string) string {
+		return revel.Config.StringDefault(key, "")
+	}
 }
