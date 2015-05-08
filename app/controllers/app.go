15c808ba1aeca10c9e9fd6f74b68260243d61cbf
Alex Xandra Albert Sim <bertzzie@gmail.com>
2015-05-08 09:34:32 +0700

    Login session OK

--- app/controllers/app.go
+++ app/controllers/app.go
@@ -20,7 +20,7 @@ func (c App) Login() revel.Result {
 	return c.Render()
 }
 
-func (c App) ProcessLogin(email, password string) revel.Result {
+func (c App) ProcessLogin(email, password string, remember bool) revel.Result {
 	contributor, uerr := c.GetContributor(email)
 
 	if uerr != nil {
@@ -29,6 +29,14 @@ func (c App) ProcessLogin(email, password string) revel.Result {
 	} else {
 		err := bcrypt.CompareHashAndPassword([]byte(contributor.Password), []byte(password))
 		if err == nil {
+			c.Session["user"] = contributor.Email
+
+			if remember {
+				c.Session.SetDefaultExpiration()
+			} else {
+				c.Session.SetNoExpiration()
+			}
+
 			loginTime := time.Now().Local().Format(revel.Config.StringDefault("format.datetime", "02 Jan 2006 15:04"))
 			revel.INFO.Printf("[LGINFO] Contributor %s logged in at %s.", email, loginTime)
 			c.Flash.Success(fmt.Sprintf(c.Message("login.message.success"), contributor.Name))
@@ -40,5 +48,6 @@ func (c App) ProcessLogin(email, password string) revel.Result {
 		}
 	}
 
+	c.Flash.Out["username"] = email
 	return c.Redirect(routes.App.Login())
 }
