8ee0a08d9e60d2b6cc72ec090d88f97eddd02c81
Alex Xandra Albert Sim <bertzzie@gmail.com>
2015-07-24 19:46:23 +0700

    First attempt at update profile form
    - We also fix the URL of /profile/edit to add id
      so we can query the data properly.

--- app/controllers/contributor.go
+++ app/controllers/contributor.go
@@ -1,14 +1,23 @@
 package controllers
 
 import (
+	"fmt"
 	"github.com/revel/revel"
-	//"obrolansubuh.com/models"
+	"obrolansubuh.com/models"
 )
 
 type Contributor struct {
 	GormController
 }
 
-func (c Contributor) EditProfile() revel.Result {
-	return c.Render()
+func (c Contributor) EditProfile(id int64) revel.Result {
+	contributor := &models.Contributor{}
+	c.Trx.Where("id = ?", id).First(&contributor)
+
+	if err := c.Trx.Error; err != nil {
+		fmt.Println("ERROR")
+		return c.RenderText("ERROR")
+	}
+
+	return c.Render(contributor)
 }
