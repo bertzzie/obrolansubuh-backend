4ce9a216c4f0767e66735d74a9faec74565bd7bd
Alex Xandra Albert Sim <bertzzie@gmail.com>
2015-07-24 20:11:26 +0700

    First implementation of handling profile update

--- app/controllers/contributor.go
+++ app/controllers/contributor.go
@@ -3,6 +3,7 @@ package controllers
 import (
 	"fmt"
 	"github.com/revel/revel"
+	"obrolansubuh.com/backend/app/routes"
 	"obrolansubuh.com/models"
 )
 
@@ -20,4 +21,42 @@ func (c Contributor) EditProfile(id int64) revel.Result {
 	}
 
 	return c.Render(contributor)
+}
+
+func (c Contributor) UpdateProfile(
+	id int64,
+	name string,
+	email string,
+	about string,
+	photo []byte) revel.Result {
+
+	contributor := &models.Contributor{}
+	c.Trx.Where("id = ?", id).First(&contributor)
+
+	contributor.Name = name
+	contributor.Email = email
+	contributor.About = about
+
+	c.Session["user"] = contributor.Email
+	c.Session["username"] = contributor.Name
+
+	if len(photo) > 0 {
+		fileName := c.Params.Files["photo"][0].Filename
+		uploadDr := revel.Config.StringDefault("upload.image.location", "/public/upload/")
+		hashName := hashFileName(fileName, c.Session["user"])
+		fullName := uploadDr + hashName
+
+		if err := saveFile(photo, revel.BasePath+fullName); err != nil {
+			c.Flash.Error("GAGAL")
+			return c.Redirect(routes.Contributor.EditProfile(id))
+		}
+
+		contributor.Photo = fullName
+		c.Session["userphoto"] = fullName
+	}
+
+	c.Trx.Save(&contributor)
+
+	c.Flash.Success("Berhasil!")
+	return c.Redirect(routes.Contributor.EditProfile(id))
 }
