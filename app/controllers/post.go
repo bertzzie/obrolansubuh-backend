5a35777f76ad59aa9b917cc42a34188eb18b6478
Alex Xandra Albert Sim <bertzzie@gmail.com>
2015-08-03 19:35:58 +0700

    Added Asset controller to manage asset.
    Main purpose is to manage things like images, video, etc
    in the future. For now, we can only upload image with it.
    More functionalities are coming alongside with more features!
    
    This was made because we need upload image in different places,
    not just post.

--- app/controllers/post.go
+++ app/controllers/post.go
@@ -1,8 +1,6 @@
 package controllers
 
 import (
-	"crypto/md5"
-	"encoding/hex"
 	"encoding/json"
 	"fmt"
 	"github.com/jinzhu/gorm"
@@ -11,7 +9,6 @@ import (
 	"net/http"
 	"obrolansubuh.com/backend/app/routes"
 	"obrolansubuh.com/models"
-	"path/filepath"
 	"strconv"
 	"strings"
 	"time"
@@ -374,77 +371,4 @@ func (c Post) Update(id int64) revel.Result {
 		Message: message}
 	c.Response.Status = http.StatusOK // should we use Created for update too?
 	return c.RenderJson(PU)
-}
-
-func (c Post) ImageUpload(image []byte) revel.Result {
-	fileType := c.Params.Files["image"][0].Header["Content-Type"]
-	fileName := c.Params.Files["image"][0].Filename
-
-	hostname := revel.Config.StringDefault("server.hostname", "http://localhost:9000")
-	uploadDr := revel.Config.StringDefault("upload.image.location", "/public/upload/")
-
-	hashName := hashFileName(fileName, c.Session["user"])
-
-	fullName := uploadDr + hashName
-
-	if err := saveFile(image, revel.BasePath+fullName); err != nil {
-		revel.ERROR.Printf("[LGFATAL] Failed to write uploaded file %s by %s. Error: %s",
-			revel.BasePath+fullName,
-			c.Session["user"],
-			err)
-
-		failedUpload := FailedUpload{
-			Name:  fullName,
-			Size:  len(image),
-			Error: c.Message("errors.upload.image"),
-		}
-
-		FUR := FileUploadError{
-			Files: []FailedUpload{failedUpload},
-		}
-
-		c.Response.Status = http.StatusInternalServerError
-		return c.RenderJson(FUR)
-	} else {
-		fileInfo := UploadedFile{
-			Url:         hostname + fullName,
-			ContentType: fileType[0],
-			ContentSize: len(image),
-			DeleteURL:   hostname + "/post/image/delete/" + fileName,
-			DeleteType:  "DELETE",
-		}
-
-		FUR := FileUploadResponse{
-			Files: []UploadedFile{fileInfo},
-		}
-
-		return c.RenderJson(FUR)
-	}
-}
-
-func saveFile(file []byte, destination string) error {
-	err := ioutil.WriteFile(destination, file, 0644) // Permission: -rw-r--r--
-
-	if err != nil {
-		return err
-	}
-
-	return nil
-}
-
-// Hash function to change filename so we always have unique
-// filename for uploaded file.
-//
-// Hash function is a simple
-func hashFileName(filename string, username string) string {
-	ext := filepath.Ext(filename)
-
-	fullname := filename + "_" + username + "_" + time.Now().Format("20060102150405")
-	sum := md5.Sum([]byte(fullname))
-	hash := hex.EncodeToString(sum[:])
-
-	result := hash + ext
-
-	return result
-
 }
