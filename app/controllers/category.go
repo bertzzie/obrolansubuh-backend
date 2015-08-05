145cadc981788f240667aed84e5c9148d0448b6e
Alex Xandra Albert Sim <bertzzie@gmail.com>
2015-08-05 20:22:40 +0700

    Category.New messages

--- app/controllers/category.go
+++ app/controllers/category.go
@@ -13,15 +13,19 @@ type Category struct {
 
 func (c Category) New() revel.Result {
 	ToolbarItems := []ToolbarItem{
-		ToolbarItem{Id: "save-category", Text: "Save", Icon: "note-add", Url: "Category.New"},
+		ToolbarItem{
+			Id:   "save-category",
+			Text: c.Message("category.button.save"),
+			Icon: "note-add",
+			Url:  "Category.New",
+		},
 	}
 
 	return c.Render(ToolbarItems)
 }
 
 func (c Category) Save(heading, description, image string) revel.Result {
-	c.Validation.Required(heading).Message("Kurang Heading")
-	c.Validation.Required(description).Message("Kurang Description")
+	c.Validation.Required(heading).Message(c.Message("category.validation.heading"))
 
 	if c.Validation.HasErrors() {
 		messages := make([]string, 0, len(c.Validation.Errors))
@@ -44,11 +48,16 @@ func (c Category) Save(heading, description, image string) revel.Result {
 	if err := c.Trx.Create(&newCat).Error; err != nil {
 		revel.ERROR.Printf("[LGFATAL] Failed to save new category in database.")
 
-		FR := FailRequest{Messages: []string{"Error database"}}
+		FR := FailRequest{Messages: []string{c.Message("errors.db.generic")}}
 
 		c.Response.Status = http.StatusInternalServerError
 		return c.RenderJson(FR)
 	}
+
+	revel.INFO.Printf("[LGINFO] Contributor %s created a category with id %d.",
+		c.Session["username"],
+		newCat.ID,
+	)
 
 	actions := JsonResponse{
 		Actions: []Action{
