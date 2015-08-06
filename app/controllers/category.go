f96159d505b86c35a0091d977e44d9ea97220394
Alex Xandra Albert Sim <bertzzie@gmail.com>
2015-08-06 16:10:54 +0700

    Edit category OK. Now moving towards list.

--- app/controllers/category.go
+++ app/controllers/category.go
@@ -3,12 +3,57 @@ package controllers
 import (
 	"github.com/revel/revel"
 	"net/http"
-	//"obrolansubuh.com/backend/app/routes"
+	"obrolansubuh.com/backend/app/routes"
 	"obrolansubuh.com/models"
+	"strconv"
 )
 
 type Category struct {
 	GormController
+}
+
+type CategoryList struct {
+	ID          int64
+	Heading     string
+	Description string
+}
+
+func (c Category) JsonList() revel.Result {
+	var categories []models.Category
+	if err := c.Trx.Find(&categories).Error; err != nil {
+
+		revel.ERROR.Printf("[LGFATAL] Failed to get category list from database. Error: %s", err)
+
+		FR := FailRequest{Messages: []string{c.Message("errors.db.generic")}}
+
+		c.Response.Status = http.StatusInternalServerError
+		return c.RenderJson(FR)
+	}
+
+	catList := make([]CategoryList, 0, len(categories))
+	for _, category := range categories {
+		tmp := CategoryList{
+			ID:          category.ID,
+			Heading:     category.Heading,
+			Description: category.Description,
+		}
+		catList = append(catList, tmp)
+	}
+
+	return c.RenderJson(catList)
+}
+
+func (c Category) List() revel.Result {
+	ToolbarItems := []ToolbarItem{
+		ToolbarItem{
+			Id:   "new-category",
+			Text: c.Message("menu.category.new"),
+			Icon: "note-add",
+			Url:  "Category.New",
+		},
+	}
+
+	return c.Render(ToolbarItems)
 }
 
 func (c Category) New() revel.Result {
@@ -62,11 +107,92 @@ func (c Category) Save(heading, description, image string) revel.Result {
 	actions := JsonResponse{
 		Actions: []Action{
 			// The Rel should be updated when we've implemented them
-			Action{Uri: "Edit", Rel: "/category/edit/1"},
-			Action{Uri: "Delete", Rel: "/category/delete/1"},
+			Action{Uri: "Edit", Rel: routes.Category.Edit(newCat.ID)},
 		},
 	}
 
 	c.Response.Status = http.StatusCreated
+	return c.RenderJson(actions)
+}
+
+func (c Category) Edit(id int64) revel.Result {
+	ToolbarItems := []ToolbarItem{
+		ToolbarItem{
+			Id:   "update-category",
+			Text: c.Message("menu.category.update"),
+			Icon: "editor:publish",
+			Url:  "Category.Edit",
+		},
+	}
+
+	var Category models.Category
+	if err := c.Trx.Where("id = ?", id).First(&Category).Error; err != nil {
+		revel.ERROR.Printf("[LGFATAL] Failed to get Category ID %d data. Error: %s",
+			id,
+			err,
+		)
+
+		c.Flash.Error(c.Message("errors.db.generic"))
+		return c.Redirect(routes.Category.List())
+	}
+
+	return c.Render(ToolbarItems, Category)
+}
+
+func (c Category) Update(id, heading, description, image string) revel.Result {
+	c.Validation.Required(id).Message(c.Message("category.validation.id"))
+	c.Validation.Required(heading).Message(c.Message("category.validation.heading"))
+
+	if c.Validation.HasErrors() {
+		messages := make([]string, 0, len(c.Validation.Errors))
+		for _, v := range c.Validation.ErrorMap() {
+			messages = append(messages, v.String())
+		}
+
+		FR := FailRequest{Messages: messages}
+
+		c.Response.Status = http.StatusBadRequest
+		return c.RenderJson(FR)
+	}
+
+	realID, iderr := strconv.ParseInt(id, 10, 64)
+	if iderr != nil {
+		revel.ERROR.Printf("[LGWRD] ID parse error. Value: %s, should be valid int64 value. Parser Error: %s",
+			id,
+			iderr,
+		)
+
+		FR := FailRequest{Messages: []string{c.Message("errors.category.parseid")}}
+
+		c.Response.Status = http.StatusBadRequest
+		return c.RenderJson(FR)
+	}
+
+	cat := models.Category{
+		Heading:     heading,
+		Description: description,
+		Image:       image,
+	}
+
+	if err := c.Trx.Table("categories").Where("id = ?", realID).Updates(&cat).Error; err != nil {
+		revel.ERROR.Printf("[LGFATAL] Error in updating new category %s. Error: %s",
+			heading,
+			err,
+		)
+
+		FR := FailRequest{Messages: []string{c.Message("errors.db.generic")}}
+
+		c.Response.Status = http.StatusInternalServerError
+		return c.RenderJson(FR)
+	}
+
+	actions := JsonResponse{
+		Actions: []Action{
+			Action{Uri: "New", Rel: routes.Category.New()},
+			Action{Uri: "List", Rel: routes.Category.List()},
+		},
+	}
+
+	c.Response.Status = http.StatusOK
 	return c.RenderJson(actions)
 }
